f023a24041929b99b493edf39f8732927b52d29e
Izaa <108992411+Izaa0@users.noreply.github.com>
2023-08-17 19:57:04 +0200

    Add files via upload

---
+++ GLFW-External-Overlay/GLFWExternalOverlay/src.cpp
@@ -0,0 +1,313 @@
+// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
+// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
+// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
+// Read online: https://github.com/ocornut/imgui/tree/master/docs
+
+
+#include <Windows.h>
+#include "GL/glew.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include <stdio.h>
+#include "helper.h"
+#include "utilities.h"
+#include "WorldToScreen.h"
+#define GL_SILENCE_DEPRECATION
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+
+// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
+// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
+// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+// This example can also compile and run with Emscripten! See 'Makefile.emscripten' for details.
+#ifdef __EMSCRIPTEN__
+#include "../libs/emscripten/emscripten_mainloop_stub.h"
+#endif
+
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+// Main code
+int main(int, char**)
+{
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+
+
+
+    const char* glsl_version = "#version 130";
+  
+
+
+    if (glewInit() != GLEW_OK)
+    {
+        fprintf(stderr, "Failed to initialize OpgenGL Loader");
+    }
+
+
+    GLFWmonitor* Monitor = glfwGetPrimaryMonitor();
+    if (!Monitor)
+    {
+        return 0;
+    }
+
+    glfwWindowHint(GLFW_FLOATING, true);
+    glfwWindowHint(GLFW_MAXIMIZED, true);
+    glfwWindowHint(GLFW_RESIZABLE, false);
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, true);
+   
+
+    int width = glfwGetVideoMode(Monitor)->width;
+    int height = glfwGetVideoMode(Monitor)->height;
+
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(width, height, "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
+    if (window == nullptr)
+        return 1;
+
+    glfwSetWindowAttrib(window, GLFW_DECORATED, false);
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+   
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    
+
+    // Our state
+    bool show_another_window = false;
+    bool bMenuVisible = true;
+
+    DWORD procId = GetProcId(L"hl2.exe");
+
+    if (procId)
+        std::cout << "Process Found:" << procId << std::endl;
+
+    uintptr_t moduleBase = GetModuleBaseAddr(procId, L"client.dll");
+
+    uintptr_t engineBase = GetModuleBaseAddr(procId, L"engine.dll");
+
+    uintptr_t localPlayer = moduleBase + 0x5044A4;
+
+    HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, 0, procId);
+
+
+    // Main loop
+#ifdef __EMSCRIPTEN__
+    io.IniFilename = nullptr;
+    EMSCRIPTEN_MAINLOOP_BEGIN
+#else
+    while (!glfwWindowShouldClose(window))
+#endif
+    {
+        
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        if (GetAsyncKeyState(VK_INSERT) & 1)
+        {
+            bMenuVisible = !bMenuVisible;
+
+
+            if (bMenuVisible)
+            {
+                ShowMenu(window);
+            }
+            else
+            {
+                HideMenu(window);
+            }
+        }
+
+
+        if(bMenuVisible)
+        {
+            //Draw menu
+            ImGui::Button("Test Button");
+            
+        }
+        else
+        {
+
+        }
+         
+
+        uintptr_t readMatrix = Memory::RPM<uintptr_t>(hProc, (engineBase + 0x5AAAA4));
+
+        ViewMatrix Matrix = Memory::RPM<ViewMatrix>(hProc, (readMatrix + 0x2D4));
+
+        uintptr_t localTeamAddr = FinDMAAddy(hProc, localPlayer, { 0x9C });
+
+        uintptr_t localTeam = Memory::RPM< uintptr_t>(hProc, localTeamAddr);
+
+        for (unsigned int i = 1; i < 32; ++i)
+        {
+            uintptr_t EntityList = Memory::RPM<uintptr_t>(hProc, (moduleBase + 0x4C49D0));
+
+            uintptr_t Entity = Memory::RPM<uintptr_t>(hProc, (EntityList + i * 0x8));
+
+
+            uintptr_t health = Memory::RPM<uintptr_t>(hProc, (Entity + 0x94));
+
+            uintptr_t liveState = Memory::RPM<uintptr_t>(hProc, (Entity + 0x93));
+
+            uintptr_t EntityTeam = Memory::RPM<uintptr_t>(hProc, (Entity + 0x9C));
+
+            uintptr_t dormant = Memory::RPM<uintptr_t>(hProc, (Entity + 0x17C));
+
+
+            Vec3 vecOrigin = Memory::RPM<Vec3>(hProc, (Entity + 0x290));
+
+            Vec3 headOrigin;
+            headOrigin.X = vecOrigin.X;
+            headOrigin.Y = vecOrigin.Y;
+            headOrigin.Z = vecOrigin.Z + 60.0f;
+
+            Vec2 headCoords;
+            Vec2 feetCoords;
+
+            if (!WorldToScreen(vecOrigin, feetCoords, Matrix.Matrix)) continue;
+
+            if (!WorldToScreen(headOrigin, headCoords, Matrix.Matrix)) continue;
+
+            float height = headCoords.Y - feetCoords.Y;
+            float width = height / 3.8f;
+
+
+            if (localTeam != EntityTeam)
+            {
+                glLineWidth(3.0f);
+                glColor3f(1.0f, 0.0f, 0.0f);
+
+                glBegin(GL_LINES);
+                glVertex2f(feetCoords.X - (width / 2), feetCoords.Y);
+                glVertex2f(feetCoords.X - (width / 2), headCoords.Y);
+                glEnd();
+
+                glBegin(GL_LINES);
+                glVertex2f(feetCoords.X - (width / 2), headCoords.Y);
+                glVertex2f(feetCoords.X + (width / 2), headCoords.Y);
+                glEnd();
+
+                glBegin(GL_LINES);
+                glVertex2f(feetCoords.X + (width / 2), headCoords.Y);
+                glVertex2f(feetCoords.X + (width / 2), feetCoords.Y);
+                glEnd();
+
+                glBegin(GL_LINES);
+                glVertex2f(feetCoords.X + (width / 2), feetCoords.Y);
+                glVertex2f(feetCoords.X - (width / 2), feetCoords.Y);
+                glEnd();
+            }
+            else
+            {
+                glLineWidth(3.0f);
+                glColor3f(0.0f, 0.0f, 1.0f);
+
+                glBegin(GL_LINES);
+                glVertex2f(feetCoords.X - (width / 2), feetCoords.Y);
+                glVertex2f(feetCoords.X - (width / 2), headCoords.Y);
+                glEnd();
+
+                glBegin(GL_LINES);
+                glVertex2f(feetCoords.X - (width / 2), headCoords.Y);
+                glVertex2f(feetCoords.X + (width / 2), headCoords.Y);
+                glEnd();
+
+
+                glBegin(GL_LINES);
+                glVertex2f(feetCoords.X + (width / 2), headCoords.Y);
+                glVertex2f(feetCoords.X + (width / 2), feetCoords.Y);
+                glEnd();
+
+                glBegin(GL_LINES);
+                glVertex2f(feetCoords.X + (width / 2), feetCoords.Y);
+                glVertex2f(feetCoords.X - (width / 2), feetCoords.Y);
+                glEnd();
+            }
+
+            if (health >= 70)
+            {
+                glColor3f(0.0f, 1.0f, 0.0f);
+                glBegin(GL_LINES);
+                glVertex2f(feetCoords.X - (width / 1.5), feetCoords.Y);
+                glVertex2f(feetCoords.X - (width / 1.5), headCoords.Y);
+                glEnd();
+            }
+            else if (health <= 70 && health >= 40)
+            {
+                glColor3f(1.0f, 1.0f, 0.0f);
+                glBegin(GL_LINES);
+                glVertex2f(feetCoords.X - (width / 1.5), feetCoords.Y);
+                glVertex2f(feetCoords.X - (width / 1.5), headCoords.Y - (width / 0.85));
+                glEnd();
+            }
+            else if (health <= 40 && health >= 0)
+            {
+                glColor3f(1.0f, 0.0f, 0.0f);
+                glBegin(GL_LINES);
+                glVertex2f(feetCoords.X - (width / 1.5), feetCoords.Y);
+                glVertex2f(feetCoords.X - (width / 1.5), headCoords.Y - (width / 0.40));
+                glEnd();
+            }
+
+        }
+
+
+        
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+#ifdef __EMSCRIPTEN__
+    EMSCRIPTEN_MAINLOOP_END;
+#endif
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
