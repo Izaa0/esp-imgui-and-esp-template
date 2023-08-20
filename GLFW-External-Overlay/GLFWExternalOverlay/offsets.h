ae43379dd779c1a568da36ea89c395fc163512b9
Izaa <108992411+Izaa0@users.noreply.github.com>
2023-08-20 07:05:45 +0200

    Update offsets.h

--- GLFW-External-Overlay/GLFWExternalOverlay/offsets.h
+++ GLFW-External-Overlay/GLFWExternalOverlay/offsets.h
@@ -5,10 +5,5 @@
 
 namespace offsets
 {
-    uintptr_t aLocalPlayer = 0x5044A4;
-    uintptr_t mEntityList = 0x004C49C0;
-
-    uintptr_t eViewMatrix = 0x5B07F8;
-    uintptr_t oVecOrigin = 0x338;
-    uintptr_t oHealth = 0x94;
+   
 }
